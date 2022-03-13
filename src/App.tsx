af8787f8515ebb2d23dc6adb94118958d33fe64a
a-GumaryaN <alireza313gomarian@gmail.com>
2022-03-13 16:39:27 +0330

    adding register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./Components/LoginPage/LoginPage";
+import Register from "./Components/Register/Register";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
